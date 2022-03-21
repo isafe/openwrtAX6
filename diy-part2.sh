526395a42ae337480026bac67e83876607ee85e8
iSec <isafe@users.noreply.github.com>
2022-03-21 10:00:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -93,7 +93,6 @@ sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package
 
 # themes添加（svn co 命令意思：指定版本如https://github）
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
-git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
 #git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
 #git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
 #git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial
