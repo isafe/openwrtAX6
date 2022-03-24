95c07091f6384034b18f8f7d88fec79771552688
iSec <isafe@users.noreply.github.com>
2022-03-24 20:59:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -103,7 +103,7 @@ git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 
 #添加额外非必须软件包
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
-git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+# git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 #git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
 #git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 # me #git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
