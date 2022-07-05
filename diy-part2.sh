70b531cd697c78864b67d008c00e2ef967663d82
iSec <isafe@users.noreply.github.com>
2022-07-05 12:07:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -94,7 +94,8 @@ sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package
 
 # themes添加（svn co 命令意思：指定版本如https://github）
 rm -rf feeds/luci/themes/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon 
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon 
+
 #rm -rf package/luci-theme-argon
 #git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 #git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
